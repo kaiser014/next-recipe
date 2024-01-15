2e1cbb58a710e767d824fab20536919b7359a701
Kaiser <kaiser@Kaisers-MacBook-Pro.local>
2024-01-16 02:31:17 +0600

    create add form

--- src/app/editRecipe/[id]/page.jsx
+++ src/app/editRecipe/[id]/page.jsx
@@ -5,7 +5,7 @@ export default function EditRecipe(){
                 <form className="flex flex-col gap-3">
                     <input type="text" className="border border-slate-500 px-8 py-2" placeholder="Recipes Title"/>
                     <input type="text" className="border border-slate-500 px-8 py-2" placeholder="Recipes Description"/>
-                    <button className="bg-green-600 text-white py-3 px-6 w-fit">Add Recipes</button>
+                    <button className="bg-green-600 text-white py-3 px-6 w-fit">Update Recipes</button>
                 </form>
             </div>
         </>
